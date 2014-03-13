6742b024fdea3170332e5c249491469eb44ff0d1
Ender <wanjiadong@gmail.com>
2014-03-13 15:33:55 +0800

    improve install-software.sh

--- bin/install-software.sh
+++ bin/install-software.sh
@@ -1,14 +1,68 @@
-#! /bin/bash
+#!/bin/bash
+
+show_help () {
+    cat <<EOF
+This script using to install some require softwares.
+
+options:
+        -a, --all            install all default software.
+EOF
+}
+
+while [ $# -gt 0 ] 
+do
+    case '$1' in
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+
+        -a|--all)
+            export INSTALL_ALL = "True"
+            shift
+            ;;
+
+        *)
+            echo "wrong options " + $1
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+isInstalled () {
+    if hash "$1" 2>/dev/null; then
+        echo 1;
+    else
+        echo 0;
+    fi
+}
 
 # install emacs 24
-sudo add-apt-repository ppa:cassou/emacs
-sudo apt-get update
+install_latest_emacs () {
+    sudo add-apt-repository ppa:cassou/emacs
+    sudo apt-get update
 
-# install require software
-sudo apt-get install emacs24 tmux
+    sudo apt-get install emacs24
+}
 
 #install leiningen
-wget -P ~/bin https://raw.github.com/technomancy/leiningen/stable/bin/lein
-chmod a+x ~/bin/lein
-~/bin/lein
+install_lein () {
+    if [ ! $(isInstalled "lein") ]; then
+        echo "installing leiningen"
 
+        wget -P ~/bin https://raw.github.com/technomancy/leiningen/stable/bin/lein
+        chmod a+x ~/bin/lein
+        ~/bin/lein
+    fi
+}
+
+# install require software
+if [ INSTALL_ALL ]; then
+
+    sudo apt-get install tmux
+
+    install_latest_emacs
+
+    install_lein
+fi
